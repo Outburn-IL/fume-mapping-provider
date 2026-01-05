f93754bdfcde72748b5d57d1eea56df715892577
mechanik-daniel <daniel@outburn.co.il>
2026-01-05 12:15:19 +0200

    feat: add built-in aliases for standard identifiers

---
+++ src/builtInAliases.ts
@@ -0,0 +1,16 @@
+import { AliasObject } from './types';
+
+export const builtInAliases: AliasObject = {
+  ucum: 'http://unitsofmeasure.org',
+  loinc: 'http://loinc.org',
+  sct: 'http://snomed.info/sct',
+  urn: 'urn:ietf:rfc:3986',
+  ssn: 'http://hl7.org/fhir/sid/us-ssn',
+  passportPrefix: 'http://hl7.org/fhir/sid/passport-',
+  extDar: 'http://hl7.org/fhir/StructureDefinition/data-absent-reason',
+  extLanguage: 'http://hl7.org/fhir/StructureDefinition/language',
+  extGeolocation: 'http://hl7.org/fhir/StructureDefinition/geolocation',
+  extStreetName: 'http://hl7.org/fhir/StructureDefinition/iso21090-ADXP-streetName',
+  extHouseNumber: 'http://hl7.org/fhir/StructureDefinition/iso21090-ADXP-houseNumber',
+  extApartment: 'http://hl7.org/fhir/StructureDefinition/iso21090-ADXP-unitID'
+};
